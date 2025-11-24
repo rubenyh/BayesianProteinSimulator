ea91e682d5cc579e00add0b4f693aeb74e1acbc1
Cedric Martinez <cedric.martinez@cetys.edu.mx>
2025-11-24 10:05:41 -0800

    very simple integration of fastapi

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter
-from schemas import SequenceInput, PredictionOutput
-from protein_model.pipeline import run_pipeline
+from api.schemas import SequenceInput, PredictionOutput
+# from protein_model.pipeline import run_pipeline
 
 router = APIRouter()
 
-@router.post("/predict", response_model=PredictionOutput)
-def predict_structure(payload: SequenceInput):
-    result = run_pipeline(payload.sequence)
-    return result
+# @router.post("/predict", response_model=PredictionOutput)
+# def predict_structure(payload: SequenceInput):
+#     result = run_pipeline(payload.sequence)
+#     return result
+
+@router.get("/hello")
+def hello():
+    return {"msg":"hello from here"}
