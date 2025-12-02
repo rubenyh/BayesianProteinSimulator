8d34df5dd1f19e9af157147ba1f4ee3bc626a445
rubenyh <rubenyanezhernandez@gmail.com>
2025-12-01 21:06:14 -0800

    changed origins

--- backend/main.py
+++ backend/main.py
@@ -8,9 +8,14 @@ app = FastAPI(
     version="1.0.0"
 )
 
+origins = [
+    "http://localhost:3000",
+    "https://bayesian-protein-simulator.vercel.app/",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
